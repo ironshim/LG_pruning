5d9739045f685b1295f0247c93e6ea3c599d0ebb
sungnyun <ksn4397@gmail.com>
2020-11-18 15:15:18 +0900

    add filter pruning

---
+++ Pruning_filters_for_efficient_convnets/main.py
@@ -0,0 +1,20 @@
+from parameter import get_parameter
+from train import train_network
+from evaluate import test_network
+from prune import prune_network
+
+# import os
+# os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
+# os.environ['CUDA_VISIBLE_DEVICES'] = '2'
+
+if __name__ == '__main__':
+    args = get_parameter()
+    
+    network = None
+    if args.train_flag:
+        network = train_network(args, network=network)
+
+    if args.prune_flag:
+        network = prune_network(args, network=network)
+
+    test_network(args, network=network)
